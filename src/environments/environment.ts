e51559b5d1fee9e3eaaee903d77fe8b89e0fe4df
vyonna <vyonna.njenga@student.moringaschool.com>
2022-02-12 21:17:20 +0300

    added github token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  apiKey: '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',
+  
 };
 
 /*
