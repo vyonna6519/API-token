8800639a37b136e78df2e19060a87e1952b798e4
vyonna <vyonna.njenga@student.moringaschool.com>
2022-02-13 22:14:18 +0300

    added repo selector to app component

--- src/app/repo/repo.component.ts
+++ src/app/repo/repo.component.ts
@@ -1,31 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { SearchRepoService } from '../search-repo.service';
-import { HttpClient } from '@angular/common/http';
-@Component({
-  selector: 'app-repo',
-  templateUrl: './repo.component.html',
-  styleUrls: ['./repo.component.css']
-})
-export class RepoComponent implements OnInit {
-  searchRepoQuery!: string;
-  repos!:any;
-  SearchRepoRequestService: any;
+  import { Component, OnInit } from '@angular/core';
+  import { SearchRepoService } from '../search-repo.service';
+  import { HttpClient } from '@angular/common/http';
+  @Component({
+    selector: 'app-repo',
+    templateUrl: './repo.component.html',
+    styleUrls: ['./repo.component.css']
+  })
+  export class RepoComponent implements OnInit {
+    searchRepoQuery!: string;
+    repos!:any;
+    SearchRepoRequestService: any;
 
-  constructor(private SearchRepoService: SearchRepoService) { }
-  search(){
-    this.searchrepo(this.searchRepoQuery)
-  }
-  searchrepo(searchRepoQuery: string) {
-    this.SearchRepoRequestService.reporequest(searchRepoQuery).subscribe(
-      (response: any) => {
-        this.repos = response.items;
-        console.log(this.repos);
-      }
-    );
-    ;
-  }
+    constructor(private SearchRepoService: SearchRepoService) { }
+    search(){
+      this.searchrepo(this.searchRepoQuery)
+    }
+    searchrepo(searchRepoQuery: string) {
+      this.SearchRepoRequestService.reporequest(searchRepoQuery).subscribe(
+        (response: any) => {
+          this.repos = response.items;
+          console.log(this.repos);
+        }
+      );
+      ;
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-}
+  }
